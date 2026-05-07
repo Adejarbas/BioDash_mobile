e0c3524892ef7f7b1657645776863d4c3ababf24
Thiago <thiago.neves7@fatec.sp.gov.br>
2026-05-07 02:20:39 -0300

    atualização de api register para signup

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -37,7 +37,7 @@ export const authLib = {
     numero?: string;
     zipCode?: string;
   }) => {
-    const res = await fetch(`${API_URL}/auth/register`, {
+    const res = await fetch(`${API_URL}/auth/signup`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data),
